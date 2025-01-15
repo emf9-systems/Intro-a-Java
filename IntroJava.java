e0fba891393268472a45c0a10b656ff7bec7f91c
Angel Olivares <aaommago@proton.me>
2025-01-14 21:01:44 -0300

    Se agregan Arreglos, Bucles, Do, HashMaps

--- IntroJava.java
+++ IntroJava.java
@@ -1,6 +1,8 @@
 // PascaCase  archivos y clases
 // camelCase  variables y metodos
 
+import java.util.ArrayList;
+import java.util.HashMap;
 public class IntroJava {
 
     // Metodo principal / main es lo unico que se ejcuta
@@ -100,6 +102,108 @@ public class IntroJava {
         saludo("Don Pollo");
         
         System.out.println(sumatoria(numeroEntero, numeroEnteroDos));
+
+        // ARREGLOS/ARRAYS
+        int[] arreglo;//DECLARACION
+        arreglo = new int[5];
+        arreglo[0] = 4;
+        arreglo[1] = 8;
+        arreglo[2] = 9;
+        arreglo[3] = 10;
+        arreglo[4] = 5;
+
+        int[] arreglo2 = {4, 8, 9, 10, 5};
+
+        System.out.println(arreglo2);
+
+        ArrayList<Integer> arreglo3 = new ArrayList<Integer>();
+
+        arreglo3.add(8);
+        arreglo3.add(6);
+        arreglo3.add(99);
+
+        System.out.println(arreglo3);
+
+        ArrayList<String> arreglo4 = new ArrayList<String>();
+
+        arreglo4.add("Quandile");
+        arreglo4.add("Dingle");
+        arreglo4.add("HERE!");
+
+        System.out.println(arreglo4);
+
+
+        System.out.println(arreglo4.get(0));
+        
+        arreglo4.set(2, "WHREHEHEHE!");
+
+        System.out.println(arreglo4);
+
+        // BUCLES / CICLOS
+        for(int i=0; i<5; i++) {
+            System.out.println("Hola!");
+        }
+
+        int j = 0;
+        while (j < 5) {
+            System.out.println("Hola2!!!");
+            j++;
+        }
+
+        int inicio = 0;
+        int fin = 5;
+        while (inicio < fin) {
+            System.out.println("oh!");
+            inicio++;
+            fin--;
+        }
+
+        int k = 10;
+        do {
+            System.out.println("HOLA3!!!");
+        } while (k < 5);
+
+        // do {
+        // imprime menu
+        // } while (no ha escrito salir)
+
+        // Mapas de Hash o HashMaps
+        HashMap<String, String> estudiante0 = new HashMap<String, String>();
+        estudiante0.put("nombre", "Quandile");
+        estudiante0.put("apellido", "Dingle");
+        estudiante0.put("lugar", "HERE!!!");
+
+        System.out.println(estudiante0.get("apellido"));
+        System.out.println(estudiante0);
+        System.out.println(estudiante0.size());
+
+        // Bucle para recorrer arreglo
+        for(int w = 0; w < arreglo.length; w++) {
+            System.out.println(arreglo[w]);
+        }
+
+        HashMap<String, String> estudiante1 = new HashMap<String, String>();
+        estudiante1.put("nombre", "Don");
+        estudiante1.put("apellido", "Pollo");
+        estudiante1.put("lugar", "Un video ma pa peldel el tiempo emepecemo");
+
+        ArrayList<HashMap<String, String>> estudiantes = new ArrayList<HashMap<String, String>>();
+        estudiantes.add(estudiante0);
+        estudiantes.add(estudiante1);
+
+        System.out.println(estudiantes);
+        System.out.println(estudiantes.get(1));
+
+        /* 3 métodos para crear un nombre de super heroe
+        
+        1.- Recibiera un nombre: Super <nombre>
+        2.- Recibiera un nombre y un color: Super <nombre> <color>
+        3.- Recibiera un nombre, color y habilidad: Super <nombre> <color> <habilidad>
+        
+        TAREA METODOS Y SOBRECARGA
+          */
+
+
     }
 
     // Permiso: public, private, protected
